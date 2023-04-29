7f8b88c81b3248b3c0d1e3c064f7fb161bee9cfb
Shivam <shivamguptanitw@gmail.com>
2023-04-29 09:08:41 +0530

    News list component created and props is set

---
+++ src/NewsApp/NewsItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class NewsItems extends Component {
+     render() {
+        let {title, description, imageURL, newsURL} = this.props;
+          return (
+               <div className="card mb-3" style={{ width: "20rem" }}>
+                    <img src={`${imageURL}`} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                         <h5 className="card-title">{title}...</h5>
+                         <p className="card-text">
+                              {description}...
+                         </p>
+                         <a href={`${newsURL}`} target="_blank" rel={`${newsURL}`} className="btn btn-sm btn-primary">
+                              Read More...
+                         </a>
+                    </div>
+               </div>
+          );
+     }
+}
